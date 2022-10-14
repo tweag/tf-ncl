870d18e645be01d1932ab557374cc27e7d9c448f
Viktor Kleen <viktor.kleen@tweag.io>
2022-10-14 13:48:09 +0000

    Fix nix tooling

--- nix/nickel_schema.nix
+++ nix/nickel_schema.nix
@@ -1,10 +1,5 @@
 { lib, runCommand, tf-ncl, jsonSchema }:
-assert lib.length (lib.attrNames jsonSchema.providers) == 1;
-let
-  provider-name = lib.head (lib.attrNames jsonSchema.providers);
-  provider = jsonSchema.providers.${provider-name};
-in
-runCommand "${provider-name}-schema.ncl"
+runCommand "schema.ncl"
 { } ''
-  ${tf-ncl}/bin/tf-ncl ${provider-name} ${provider.version} <${jsonSchema} > $out
+  ${tf-ncl}/bin/tf-ncl ${jsonSchema}/providers.json ${jsonSchema}/schema.json > $out
 ''
