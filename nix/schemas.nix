f22243edb503b1a2f4400e433ef9b0300ff34fb3
Viktor Kleen <viktor.kleen@tweag.io>
2022-10-18 08:49:40 +0000

    get rid of nix/schemas.nix

--- nix/schemas.nix
+++
@@ -1,4 +0,0 @@
-{
-  aws = { source = "registry.terraform.io/hashicorp/aws"; version = "4.34.0"; outputHash = "sha256-sFjOnxKH4fdoy9qFjteYSQF5EWnscuPhrbyOaZYQU9k="; };
-  azurerm = { source = "registry.terraform.io/hashicorp/azurerm"; version = "3.26.0"; outputHash = "sha256-fUwkJEMxSZb8dLGpO5bCBBnBbOkbDW4zasA3uTafPK4="; };
-}
