c703d0d101230e1a8db2cdd6d94db7abfbae9eb6
Viktor Kleen <viktor.kleen@tweag.io>
2023-01-11 08:16:18 +0000

    Implement schema-fetch deserialization

--- tf-ncl/src/main.rs
+++ tf-ncl/src/main.rs
@@ -16,87 +16,113 @@ use tf_ncl::{
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(value_name = "PROVIDERS")]
-    providers: PathBuf,
+    // #[arg(value_name = "PROVIDERS")]
+    // providers: PathBuf,
     #[arg(value_name = "TERRAFORM-SCHEMA")]
-    schema: Option<PathBuf>,
+    // schema: Option<PathBuf>,
+    schema: PathBuf,
 }
 
-fn get_providers(opts: &Args) -> anyhow::Result<Providers> {
-    Ok(serde_json::from_reader(std::fs::File::open(
-        &opts.providers,
-    )?)?)
-}
-
-fn get_schema(opts: &Args) -> anyhow::Result<TFSchema> {
-    let schema_reader: Box<dyn Read> = if let Some(path) = &opts.schema {
-        Box::new(std::fs::File::open(path)?)
-    } else {
-        Box::new(std::io::stdin())
-    };
-
-    Ok(serde_json::from_reader(schema_reader)?)
-}
-
-struct RenderableSchema<'a>(BoxDoc<'a>);
-
-impl<'a> RenderableSchema<'a> {
-    fn render(&self, f: &mut impl io::Write) -> anyhow::Result<()> {
-        write!(f,
-"let remove_if_exists = fun key r =>
-      if builtin.is_record r && record.has_field key r
-      then record.remove key r
-      else r
-    in
-let maybe_record_map = fun f v =>
-      if builtin.is_record v
-      then record.map f v
-      else v
-    in
-let addIdField__ = fun l x =>
-      x |> record.map (fun res_type r =>
-      r |> record.map (fun name r => r & {{ \"id\" | force = \"${{%{{res_type}}.%{{name}}.id}}\" }}))
-    in
-{{
-    Config = {{
-        config | Schema,
-        renderable_config = mkConfig config,
-        ..
-    }},
-    Schema = 
-{schema},
-    mkConfig | Schema -> {{_: Dyn}}
-             = (maybe_record_map (fun k v =>
-                v |> maybe_record_map (fun res_type v =>
-                  v |> maybe_record_map (fun res_name v =>
-                    v |> remove_if_exists \"id\")))),
-}}", schema = self)?;
-        Ok(())
-    }
-}
-
-impl<'a> From<DocBuilder<'a, BoxAllocator>> for RenderableSchema<'a> {
-    fn from(d: DocBuilder<'a, BoxAllocator>) -> Self {
-        RenderableSchema(d.into_doc())
-    }
-}
-
-impl<'a> fmt::Display for RenderableSchema<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.0.render_fmt(80, f)
-    }
-}
+// fn get_providers(opts: &Args) -> anyhow::Result<Providers> {
+//     Ok(serde_json::from_reader(std::fs::File::open(
+//         &opts.providers,
+//     )?)?)
+// }
+//
+// fn get_schema(opts: &Args) -> anyhow::Result<TFSchema> {
+//     let schema_reader: Box<dyn Read> = if let Some(path) = &opts.schema {
+//         Box::new(std::fs::File::open(path)?)
+//     } else {
+//         Box::new(std::io::stdin())
+//     };
+//
+//     Ok(serde_json::from_reader(schema_reader)?)
+// }
+//
+// struct RenderableSchema<'a>(BoxDoc<'a>);
+//
+// impl<'a> RenderableSchema<'a> {
+//     fn render(&self, f: &mut impl io::Write) -> anyhow::Result<()> {
+//         write!(f,
+// "let remove_if_exists = fun key r =>
+//       if builtin.is_record r && record.has_field key r
+//       then record.remove key r
+//       else r
+//     in
+// let maybe_record_map = fun f v =>
+//       if builtin.is_record v
+//       then record.map f v
+//       else v
+//     in
+// let addIdField__ = fun l x =>
+//       x |> record.map (fun res_type r =>
+//       r |> record.map (fun name r => r & {{ \"id\" | force = \"${{%{{res_type}}.%{{name}}.id}}\" }}))
+//     in
+// {{
+//     Config = {{
+//         config | Schema,
+//         renderable_config = mkConfig config,
+//         ..
+//     }},
+//     Schema =
+// {schema},
+//     mkConfig | Schema -> {{_: Dyn}}
+//              = (maybe_record_map (fun k v =>
+//                 v |> maybe_record_map (fun res_type v =>
+//                   v |> maybe_record_map (fun res_name v =>
+//                     v |> remove_if_exists \"id\")))),
+// }}", schema = self)?;
+//         Ok(())
+//     }
+// }
+//
+// impl<'a> From<DocBuilder<'a, BoxAllocator>> for RenderableSchema<'a> {
+//     fn from(d: DocBuilder<'a, BoxAllocator>) -> Self {
+//         RenderableSchema(d.into_doc())
+//     }
+// }
+//
+// impl<'a> fmt::Display for RenderableSchema<'a> {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         self.0.render_fmt(80, f)
+//     }
+// }
 
 fn main() -> anyhow::Result<()> {
     let opts = Args::parse();
 
-    let providers = get_providers(&opts)?;
-    let schema = get_schema(&opts)?;
+    // let providers = get_providers(&opts)?;
 
-    let intermediate = intermediate::Schema::try_from(schema.with_providers(providers))
-        .map_err(|_| anyhow!("Could not construct intermediate representation"))?;
+    let go_schema: intermediate::GoSchema =
+        serde_json::from_reader(std::fs::File::open(&opts.schema)?)?;
 
-    let doc: RenderableSchema = intermediate.as_nickel().pretty(&BoxAllocator).into();
+    // let go_schema = intermediate::GoSchema([
+    //     ("data".to_string(), intermediate::Attribute{
+    //         description: Some("Dummy".to_string()),
+    //         optional: true,
+    //         interpolation: intermediate::InterpolationStrategy::Nickel,
+    //         type_: intermediate::Type::Object([
+    //             ("test".to_string(), intermediate::Attribute{
+    //                 description: Some("nested dummy".to_string()),
+    //                 optional: true,
+    //                 interpolation: intermediate::InterpolationStrategy::Nickel,
+    //                 type_: intermediate::Type::Dynamic,
+    //             })
+    //         ].into()),
+    //     })
+    // ].into());
 
-    doc.render(&mut stdout())
+    println!("{go_schema:#?}");
+    serde_json::to_writer_pretty(std::io::stdout(), &go_schema)?;
+
+    Ok(())
+
+    // let schema = get_schema(&opts)?;
+    //
+    // let intermediate = intermediate::Schema::try_from(schema.with_providers(providers))
+    //     .map_err(|_| anyhow!("Could not construct intermediate representation"))?;
+    //
+    // let doc: RenderableSchema = intermediate.as_nickel().pretty(&BoxAllocator).into();
+    //
+    // doc.render(&mut stdout())
 }
