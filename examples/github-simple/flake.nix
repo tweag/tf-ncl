c33a78d26416f627979d281505d159a59c341e62
Viktor Kleen <viktor.kleen@tweag.io>
2023-05-03 10:43:34 +0000

    Add a simplified github example (#50)

---
+++ examples/github-simple/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    tf-ncl.url = "github:tweag/tf-ncl";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://tweag-nickel.cachix.org" ];
+    extra-trusted-public-keys = [ "tweag-nickel.cachix.org-1:GIthuiK4LRgnW64ALYEoioVUQBWs0jexyoYVeLDBwRA=" ];
+  };
+
+  outputs = inputs: inputs.utils.lib.eachDefaultSystem (system:
+    {
+      devShell = inputs.tf-ncl.lib.${system}.mkDevShell {
+        providers = p: {
+          inherit (p) github;
+        };
+      };
+    });
+}
